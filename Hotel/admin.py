7938a72656c399ef3525291c01f48d98a488bdb2
afurkanyegin <abdullahfurkanyegin@outlook.com>
2020-07-20 13:03:23 +0300

    dorduncu commit

--- Hotel/admin.py
+++ Hotel/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from Hotel.models import Category, Hotel
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display= ['title', 'status']
+    list_filter = ['status']
+
+class HotelAdmin(admin.ModelAdmin):
+    list_display = ['title','category','status','bosoda']
+    list_filter = ['category']
+
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Hotel,HotelAdmin)
--- Hotel/models.py
+++ Hotel/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    STATUS=(('True','Evet'),('False','Hayır'),)
+    parent =models.ForeignKey('self',blank=True,null=True,related_name='children',on_delete=models.CASCADE)
+    title=models.CharField(max_length=150)
+    keywords=models.CharField(max_length=255)
+    description=models.CharField(max_length=255)
+    image=models.ImageField(blank=True,upload_to='images/')
+    status=models.CharField(max_length=10,choices=STATUS)
+    slug=models.SlugField()
+    create_at=models.DateTimeField(auto_now_add=True)
+    update_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Hotel(models.Model):
+    STATUS=(('True','Evet'),('False','Hayır'),)
+
+    category =models.ForeignKey(Category,on_delete=models.CASCADE)
+    title=models.CharField(max_length=150)
+    keywords=models.CharField(max_length=255)
+    description=models.CharField(max_length=255)
+    image=models.ImageField(blank=True,upload_to='images/')
+    price=models.FloatField()
+    bosoda=models.IntegerField()
+    minamount=models.IntegerField()
+    #detail=RichTextUploadingField()
+    status=models.CharField(max_length=10,choices=STATUS)
+    slug=models.SlugField()
+    create_at=models.DateTimeField(auto_now_add=True)
+    update_at=models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.title
+
